780c83a3983f0d3b94d2ade498413ff673e7a2e0
Andrei Scafi <andreiscafi@hotmail.com>
2023-04-27 17:21:27 -0300

    Creating the HTML structure.

---
+++ script.js
@@ -0,0 +1,10 @@
+/* 
+Tetris game made with HTML, CSS and Javascript, following the tutorial provided by freeCodeCamp https://www.youtube.com/watch?v=rAUn1Lom6dw, taught by teacher Ania Kubow.
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    //The code of the whole project must be writen inside this addEventListener function. 
+
+
+
+});
